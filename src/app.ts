eb7ab01b94f274452f6044b7c9494b8afba17bbc
Merajul Hasan <merajulhasan09@gmail.com>
2024-12-24 21:39:57 +0600

    added middlewares and errors folder

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import router from './app/routes';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
 const app: Application = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.use('/api', router);
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Blog server is running');
