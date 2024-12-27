7d5777e1dc5ec59bf00d8bddf55d5dd872813815
Merajul Hasan <merajulhasan09@gmail.com>
2024-12-27 18:35:28 +0600

    added cors origin

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
 app.use('/api', router);
 
