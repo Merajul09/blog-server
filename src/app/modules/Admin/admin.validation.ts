f9ab00faa6c21c8d5faf8be83f0f78fd5f6c0712
Merajul Hasan <merajulhasan09@gmail.com>
2025-01-03 17:59:51 +0600

    added admin validation

---
+++ src/app/modules/Admin/admin.validation.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+const createUserNameValidationSchema = z.object({
+  firstName: z.string().min(1).max(20),
+  middleName: z.string().max(20),
+  lastName: z.string().max(20),
+});
